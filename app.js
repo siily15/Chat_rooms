90c1e7cd45742b12e5803047dcbdb44518635faf
siily <arnokosk15@gmail.com>
2022-05-10 09:40:42 +0300

    added app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+{
+    "apps" : [{
+        "name": "triangel",
+        "script": "server.js",
+        "watch": true,
+        "cwd": "/data01/virt83022/domeenid/www.tak19kosk.itmajakas.ee/Chat_Rooms"
+    }]
+}
